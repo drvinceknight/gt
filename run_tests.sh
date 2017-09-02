4987c351c22d0ca16cead3d6bc29ae563e3ffffa
Vince Knight <vincent.knight@gmail.com>
2017-09-02 08:27:29 +0100

    Remove lax option in nb tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 # Test the notebooks and the documentation.
 # Using nbval-lax to only test that they pass.
-py.test --nbval --current-env --nbval-lax --doctest-glob="*.rst"
+py.test --nbval --current-env --doctest-glob="*.rst"
